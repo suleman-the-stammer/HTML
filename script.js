3a19ce525444f617bb2d327283ca4446f0036ef0
stammer1 <stammer786@gmail.com>
2025-01-04 23:20:35 +0500

    intial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 99;
+let b = "Stammer have power"
+console.log(a,b);
+
+let s = {
+    'name' : "Stammer",
+    "Job Title " : "Mern Stack",
+    "Salary" : 76090
+}
+console.log(s);
+console.log(typeof s);
